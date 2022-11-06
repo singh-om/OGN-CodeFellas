ac4357295e1e51da6972638e5801400d380322f8
singh-om <111462257+singh-om@users.noreply.github.com>
2022-11-06 12:37:38 +0530

    fix for defect 3355

--- src/master.java
+++ src/master.java
@@ -1,3 +1,19 @@
-This is non-editable code.
-This is the code priority.
-ye third line hai
+public class RightTrianglePattern   
+{   
+public static void main(String args[])   
+{   
+int i, j, row=6;   
+
+for(i=0; i<row; i++)   
+{   
+
+for(j=0; j<=i; j++)   
+{   
+   
+System.out.print("* ");   
+}   
+
+System.out.println();   
+}   
+}   
+}  
